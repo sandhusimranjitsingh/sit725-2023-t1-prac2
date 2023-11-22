aef43556609b709d24fe5ee2285d626a34fdd7fc
simranjitsinghsandhu1996@gmail.com <simranjitsinghsandhu1996@gmail.com>
2023-11-22 20:02:22 +1100

    first commit

---
+++ server_get.js
@@ -0,0 +1,38 @@
+
+    const express= require("express");
+    const res = require("express/lib/response");
+    const app= express();
+    const addTwoNumber= (n1,n2) => {
+        return n1+n2;
+    }
+    const subtractTwoNumber = (n1,n2) =>{
+        return n1-n2;
+    }
+    app.get("/addTwoNumber", (req,res)=>{
+        const n1= parseInt(req.query.n1);
+        const n2=parseInt(req.query.n2);
+        const result = addTwoNumber(n1,n2);
+        res.json({statuscocde:200, data: result }); 
+    });
+
+    app.get("/subtractTwoNumber", (req,res)=>{
+        const n1= parseInt(req.query.n1);
+        const n2=parseInt(req.query.n2);
+        const result = subtractTwoNumber(n1,n2);
+        res.json({statuscocde:200, data: result }); 
+    });
+
+    app.post("/addTwoNumber", (req, res) => {
+        const { n1, n2 } = req.body;
+        const result = addTwoNumber(parseInt(n1), parseInt(n2));
+        res.json({ statuscode: 300, data: result });
+    });
+    
+
+    console.log (addTwoNumber(19,12));
+    console.log (subtractTwoNumber(19,12));
+    const port=3040;
+    app.listen(port,()=> {
+        console.log("hello i'm listening to port"+port);
+    })
+    
